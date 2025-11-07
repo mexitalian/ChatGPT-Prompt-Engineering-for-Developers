c973c33ac83bcd7dc81a339d4e786747ee2fc53b
Carlo Marasco <mexitalian@gmail.com>
2025-11-07 13:08:13 +0100

    Adds setup boilerplate fill to reduce repetition accross notebooks and centralise any changes

---
+++ setup_boilerplate.py
@@ -0,0 +1,22 @@
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+client = openai.OpenAI()
+
+def get_completion(prompt, model="gpt-4o-mini"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.chat.completions.create(
+        model=model,
+        messages = [
+        {
+          "role": "system",
+          "content": "You are a concise assistant. Always answer directly without commentary, meta references, or explanations unless explicitly asked."
+        },
+        {"role": "user", "content": prompt}
+    ],
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message.content
